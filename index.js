dbd64a5b6cf84147f86266026d32a2df2a7f353a
00062816 <00062816@uca.edu.sv>
2018-06-20 23:10:17 -0600

    Renaming error testing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+//Establece todo el contenido
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
